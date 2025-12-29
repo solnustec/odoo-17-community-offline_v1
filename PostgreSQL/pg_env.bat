b882611c3962ec890dd67e6a9b32c57092b9239b
Maria <mapaute2@utpl.edu.ec>
2025-12-29 11:25:10 -0800

    init: offline odoo 17 source setup

---
+++ PostgreSQL/pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="C:\Program Files\Odoo 17.0.20251229\PostgreSQL\bin";%PATH%
+@SET PGDATA=C:\Program Files\Odoo 17.0.20251229\PostgreSQL\data
+@SET PGDATABASE=postgres
+@SET PGUSER=openpg
+@SET PGPORT=5432
+@SET PGLOCALEDIR=C:\Program Files\Odoo 17.0.20251229\PostgreSQL\share\locale
+
+                            
